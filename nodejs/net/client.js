b783b0799ad86d41f120e730c5a43cb8cc28e6d3
최영철 <veldise@gmail.com>
2014-04-10 13:08:20 +0900

    net 예제 수정

--- nodejs/net/client.js
+++ nodejs/net/client.js
@@ -1,41 +1,41 @@
-var Socket = require('net').Socket;
+var net = require('net');
 
-var localhost = '127.0.0.1'
-  , localport = '3030';
+var localhost = '127.0.0.1',
+    localport = '3030';
 
-var socket = new Socket();
-
+// var socket = new Socket();
+var socket = net.createConnection(localport, localhost);
 socket
-	.on('readable', function () {
-		console.log('Event: "readable"');
-		var buf = socket.read();
+    .on('readable', function() {
+        console.log('Event: "readable"');
+        var buf = socket.read();
 
-		if (buf) {
-			var data = buf.toString();
-			console.log('Recv[' + data.length + ']: ' + data);
-		}
-		socket.end();
-	})
-	.on('end', function () {
-		console.log('Event: "end"');
-		// The other end of the socket sends a FIN packet.
-	})
-	.on('error', function (exception) {
-		console.log('Event: "error"');
-		console.error(exception);
-	})
-	.on('drain', function () {
-		console.log('Event: "drain"');
-		// Write buffer is empty.
-	})
-	.on('close', function (had_error) {
-		console.log('Event: "close"');
-		if (had_error) {
-			// The socket had a transmission error.
-		}
-	})
-	.connect(localport, localhost, function () {
-		console.log('connect success.');
+        if (buf) {
+            var data = buf.toString();
+            console.log('Recv[' + data.length + ']: ' + data);
+        }
+        socket.end();
+    })
+    .on('end', function() {
+        console.log('Event: "end"');
+        // The other end of the socket sends a FIN packet.
+    })
+    .on('error', function(exception) {
+        console.log('Event: "error"');
+        console.error(exception);
+    })
+    .on('drain', function() {
+        console.log('Event: "drain"');
+        // Write buffer is empty.
+    })
+    .on('close', function(had_error) {
+        console.log('Event: "close"');
+        if (had_error) {
+            // The socket had a transmission error.
+        }
+    })
+    .on('connect', function() {
+        console.log('connect success.');
 
-		socket.write('Hello TCP Socket!!');
-	});
+        socket.write('Hello TCP Socket!!');
+    });
--- nodejs/net/server.js
+++ nodejs/net/server.js
@@ -1,43 +1,41 @@
 var net = require('net');
 
 var server = net.createServer();
+server.on('connection', function(client) {
+    console.log('client connected.');
 
-server.on('connection', function (client) {
-	console.log('client connected.');
+    client
+        .on('readable', function() {
+            console.log('Event: "readable"');
+            var buf = client.read();
 
-	client
-		.on('readable', function () {
-			console.log('Event: "readable"');
-			var buf = client.read();
+            if (buf) {
+                var data = buf.toString();
+                console.log('Recv[' + data.length + ']: ' + data);
 
-			if (buf) {
-				var data = buf.toString();
-				console.log('Recv[' + data.length + ']: ' + data);
-
-				console.log('Echo: ' + data);
-				client.write(data);
-			}
-		})
-		.on('end', function () {
-			console.log('Event: "end"');
-			// The other end of the socket sends a FIN packet.
-		})
-		.on('error', function (exception) {
-			console.log('Event: "error"');
-			console.error(exception);
-		})
-		.on('drain', function () {
-			console.log('Event: "drain"');
-			// Write buffer is empty.
-		})
-		.on('close', function (had_error) {
-			console.log('Event: "close"');
-			if (had_error) {
-				// The socket had a transmission error.
-			}
-		});
-});
-
-server.listen(3030, function () {
-	console.log('server listening: ' + 3030);
+                console.log('Echo: ' + data);
+                client.write(data);
+            }
+        })
+        .on('end', function() {
+            console.log('Event: "end"');
+            // The other end of the socket sends a FIN packet.
+        })
+        .on('error', function(exception) {
+            console.log('Event: "error"');
+            console.error(exception);
+        })
+        .on('drain', function() {
+            console.log('Event: "drain"');
+            // Write buffer is empty.
+        })
+        .on('close', function(had_error) {
+            console.log('Event: "close"');
+            if (had_error) {
+                // The socket had a transmission error.
+            }
+        });
+})
+.listen(3030, function() {
+    console.log('server listening: ' + 3030);
 });
